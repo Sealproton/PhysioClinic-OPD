b96d0213ed9c3f8f7895091b59597a12d3ed6df2
sealproton <sealproton@gmail.com>
2023-10-17 22:34:14 +0700

    feat(Auth): add Serialize interceptor and  UserDto

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { Serialize } from 'src/Interceptors/serialize.interceptor';
+import { UserDto } from 'src/user/dto/user.dto';
 @Controller('auth')
+@Serialize(UserDto)
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
