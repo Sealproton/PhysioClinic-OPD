12d7d9457627077094f42ab9be52f7d6152ade05
sealproton <sealproton@gmail.com>
2023-10-17 11:36:07 +0700

    feat: add controller route handler and DTO

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {}
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+@Controller('auth')
+export class UserController {
+  constructor() {}
+  @Post('signup')
+  userSignup(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+  @Post('signin')
+  userSignin(@Body() body: CreateUserDto) {
+    console.log(body);
+  }
+}
